4c242899b71e1c9bcbb48cd2ca6d461668222124
AdrianTan914 <bridleland@gmail.com>
2019-02-17 00:15:42 -0500

    setup Apollo Client

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import './styles/index.css';
+// importing the reequired dependencies from the installed packages 
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// connect ApolloClient instance with the GraphQL API 
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000'
+});
+
+// instantiate ApolloClient 
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+    <ApolloProvider>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'));
